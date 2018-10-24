fed8141279d1a00a12562494611d08da4f8f531b
mattfurness <matt.furness@gmail.com>
2018-10-24 10:42:21 +1000

    Alias improvements

--- shell/aliases/common.sh
+++ shell/aliases/common.sh
@@ -1,13 +1,11 @@
 alias fcd='cd $(fd -t d | fzf)'
-alias hist='cat ~/.bash_history | ag '
-alias ls='ls -Gla'
+alias ls='exa -la'
+alias cat='bat'
 alias npi='docker-compose down && direnv allow && docker-compose up -d'
-alias nvim="nvim -u ~/.vimrc"
+alias nvim='nvim -u ~/.vimrc'
 alias reload='fresh && source ~/.bash_profile'
 alias servedir='python -m SimpleHTTPServer 8000'
-alias short='alias | ag'
 alias sym='ln -s '
 alias uuid='uuidgen | tr "[:upper:]" "[:lower:]" | tr -d "\n" | pbcopy'
-alias vim="nvim"
-alias xls="exa -Gla --git"
+alias vim='nvim'
 export EDITOR="nvim -u ~/.vimrc"
