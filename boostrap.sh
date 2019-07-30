1ef784266bba231e6e9e2e6a7f3b9d2a3a2b9db1
mattfurness <matt.furness@gmail.com>
2019-07-30 15:17:51 +1000

    Add fzf install to bootstrap

--- boostrap.sh
+++ boostrap.sh
@@ -26,3 +26,4 @@ curl https://nixos.org/nix/install | sh
 mkdir ~/.vim/undodir
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+$(brew --prefix)/opt/fzf/install
