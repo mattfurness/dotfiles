637c486bd909ab8a16de8afba2eec157ccb2ae10
mattfurness <matt.furness@gmail.com>
2019-09-12 11:02:47 +1000

    Use startship as prompt

--- shell/prompt/git.sh
+++ shell/prompt/git.sh
@@ -1,4 +1,1 @@
-if [ -f "$(brew --prefix bash-git-prompt)/share/gitprompt.sh" ]; then
-  GIT_PROMPT_THEME=Default
-  source "$(brew --prefix bash-git-prompt)/share/gitprompt.sh"
-fi
+eval "$(starship init bash)"
