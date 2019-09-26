5b4b5323cb77750a8d6b986db76f9499028ae890
mattfurness <matt.furness@gmail.com>
2019-09-27 08:25:39 +1000

    Source fzf completion after setting vi mode

--- shell/completion/brew-completion.sh
+++ shell/completion/brew-completion.sh
@@ -1,4 +1,3 @@
 if [ -f $(brew --prefix)/etc/bash_completion ]; then
-    . $(brew --prefix)/etc/bash_completion
+  . $(brew --prefix)/etc/bash_completion
 fi
-[ -f ~/.fzf.bash ] && source ~/.fzf.bash
